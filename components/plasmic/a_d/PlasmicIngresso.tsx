d1f99bb9a032407f360db8ce52b243ba10f93b34
Samuel Santos <samuel.fg96@gmail.com>
2026-01-08 10:32:54 -0300

    remove plasmic deps

--- components/plasmic/a_d/PlasmicIngresso.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-export interface PlasmicIngressoProps {}
-
-export const PlasmicIngresso: React.FC<PlasmicIngressoProps> = () => {
-  return <div>Ingresso</div>;
-};
